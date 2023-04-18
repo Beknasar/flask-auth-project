51f4c82264c9e0e4890e6c630bec5576ab16daf7
Beknasar <680633@gmail.com>
2023-04-18 22:22:32 +0300

    Added flashing message in login route after data validation. Added check and flashing message for an existing user in register route.

--- main.py
+++ main.py
@@ -38,41 +38,56 @@ def home():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == "POST":
+        if User.query.filter_by(email=request.form.get('email')).first():
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
         hash_and_salted_password = generate_password_hash(
             request.form['password'],
             method='pbkdf2:sha256',
             salt_length=8
         )
         new_user = User(
-            email=request.form['email'],
+            email=request.form.get('email'),
+            name=request.form.get('name'),
             password=hash_and_salted_password,
-            name=request.form['name']
         )
         db.session.add(new_user)
         db.session.commit()
 
         # Log in and authenticate user after adding details to database.
         login_user(new_user)
-
+        flash('You were successfully logged in')
         return redirect(url_for('secrets'))
+
     return render_template("register.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == "POST":
-        email = request.form['email']
-        password = request.form['password']
+        email = request.form.get('email')
+        password = request.form.get('password')
 
         # Find user by email entered.
         user = db.session.query(User).filter_by(email=email).first()
 
-        # Check stored password hash against entered password hashed.
-        if check_password_hash(user.password, password):
+        # Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        # Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        # Email exists and password correct
+        else:
             login_user(user)
             return redirect(url_for('secrets'))
 
-    return render_template("login.html")
+    return render_template("login.html", error=error)
 
 
 @app.route('/secrets')
